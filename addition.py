7499567551099e77cbf44a24bc9934df510590ef
ayush <ayush@gmail.com>
2025-02-09 13:44:14 +0000

    calculator is added

--- addition.py
+++ addition.py
@@ -1,7 +1,6 @@
- a =int(input("Enter a first number:"))
- b=int(input("Enter a second number:"))
+a=int(input("Enter a first number:"))
+b=int(input("Enter a second number:"))
 
- c=a+b
-
- print(f"Sum of 2 number is {c}")
+c=a+b
+print(f"Sum of 2 number is {c}")
 
