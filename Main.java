591a590ca5b031a23c2d7e714d54c629a327007a
ayush-5345 <awasthiayush1804@gmail.com>
2025-02-09 06:09:28 -0800

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-  public static void main(String[] args) {
-    int x = 5;
-    int y = 6;
-    int sum = x + y;
-    System.out.println(sum); // Print the sum of x + y
-  }
-}
-
